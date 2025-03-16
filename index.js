b56ed5d5efff791892beaa8cffb20570bb78341c
ntwarifrank <ntwarifrank100@gmail.com>
2025-03-16 06:09:48 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require("axios");
-const dotenv = require("dotenv");
+import express from "express"
+import cors from "cors"
+import axios from "axios";
+import dotenv from "dotenv"
 
 const app = express();
 dotenv.config();
